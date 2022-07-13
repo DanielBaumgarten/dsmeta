b3416e6493a05728779fb1285f71312f2d51f957
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-13 12:07:55 -0300

    <feat>: <inclusão das imagens do projeto>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import logo from './logo.svg';
 
 function App() {
+  const [count, setCount]= useState(0);
+
   return(
-  <h1>Teste</h1>)
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count +1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.txt</code> and save to test HMR updates
+        </p>
+        <a        
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn 
+        </a>
+        {' | '}
+        <a
+          className="App-link"
+          href="https://vitaejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
