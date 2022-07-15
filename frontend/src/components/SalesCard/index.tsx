2907f9e758c679e591ae51c5890549a435611747
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2022-07-15 16:10:50 -0300

    <chore>: <data com resposta dinâmica>

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -18,12 +18,16 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`)
+
+        const dateMin = minDate.toISOString().slice(0, 10);
+        const dateMax = maxDate.toISOString().slice(0, 10);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dateMin}&maxDate=${dateMax}`)
             .then(response => {
                 setSales(response.data.content);
             })
 
-    }, []);
+    }, [minDate,maxDate]);
 
     return (
 
